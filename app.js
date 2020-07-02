23d22eb83b7057907633dd00ed0ee284bc01982f
Cynthia2019 <ccynthia2019@gmail.com>
2020-07-02 22:38:09 +0800

    try

--- app.js
+++ app.js
@@ -4,13 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var uri = 'mongodb+srv://Cynthia:CA2019zryg@@cluster-tutormatchup.oshci.mongodb.net/tutorMatchUp?retryWrites=true&w=majority'
-const port = '3001'
+const port = process.env.PORT || '3001'
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
 //connect to database 
 var db = require('./db')
 db.connect(uri, function(err) {
@@ -18,7 +17,7 @@ db.connect(uri, function(err) {
     console.log('Unable to connect to Mongo.')
     process.exit(1)
   } else {
-    app.listen(3001, function() {
+    app.listen(port, function() {
       console.log('Listening on port 3001...')
     })
   }
@@ -35,6 +34,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+//check if using heroku 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('/Users/yujiaxie/Desktop/northwestern/dot_dev/tutor/build'))
+    app.get('*', (req, res)=>{
+        res.sendFile('/Users/yujiaxie/Desktop/northwestern/dot_dev/tutor/build/index.html')
+    })
+}
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
