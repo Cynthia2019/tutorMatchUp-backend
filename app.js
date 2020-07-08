9dd939057862b65b446e2ba8cf33de98aebe4ea1
Cynthia2019 <ccynthia2019@gmail.com>
2020-07-08 16:12:29 +0800

    without client

--- app.js
+++ app.js
@@ -31,9 +31,8 @@ var app = express();
 //connect to database and start the server
 var db = require('./db')
 db.connect(process.env.MONGODB_URI || uri, function(err) {
-  console.log(process.env.MONGODB_URI)
   if (err) {
-    console.log('Cannot connect to MongoDB', error)
+    console.log('Cannot connect to MongoDB', err)
     process.exit(1)
   } else {
     app.listen(port, function() {
@@ -78,20 +77,20 @@ if (process.env.NODE_ENV !== 'production') {
 
 
 // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
 // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 
 //module.exports = app;
